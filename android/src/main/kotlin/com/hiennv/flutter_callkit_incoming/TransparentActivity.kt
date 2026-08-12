c0fdbc25d27b7e6012c60660a13d471b2d146278
mechtech-mind <aayushsaurabh02@gmail.com>
2026-08-12 17:55:39 +0530

    added callback for callnotifservice

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/TransparentActivity.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/TransparentActivity.kt
@@ -5,22 +5,31 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import com.wowphonedev.softphone.CallNotificationService
 
 class TransparentActivity : Activity() {
 
     companion object {
         var isVisible: Boolean = false
 
-        fun getIntent(context: Context, action: String, data: Bundle?): Intent {
-            val intent = Intent(context, TransparentActivity::class.java)
+        fun getIntent(
+            context: Context,
+            action: String,
+            data: Bundle?
+        ): Intent {
+            val intent = Intent(
+                context,
+                TransparentActivity::class.java
+            )
+
             intent.action = action
             intent.putExtra("data", data)
+
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
             intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+
             return intent
         }
     }
@@ -34,42 +43,73 @@ class TransparentActivity : Activity() {
         super.onCreate(savedInstanceState)
 
         val action = intent.action
+
         if (action == null) {
-            Log.w("TransparentActivity", "Intent action is null, finishing activity")
+            Log.w(
+                "TransparentActivity",
+                "Intent action is null, finishing activity"
+            )
             finish()
             return
         }
 
         val data = intent.getBundleExtra("data")
 
-        // Original broadcast behavior
+        // Original flutter_callkit_incoming flow
         val broadcastIntent =
-            CallkitIncomingBroadcastReceiver.getIntent(this, action, data)
+            CallkitIncomingBroadcastReceiver.getIntent(
+                this,
+                action,
+                data
+            )
 
-        broadcastIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND)
+        broadcastIntent.addFlags(
+            Intent.FLAG_RECEIVER_FOREGROUND
+        )
+
         sendBroadcast(broadcastIntent)
 
-        // Original app launch behavior
+        // Original application Activity
         val activityIntent =
-            AppUtils.getAppIntent(this, action, data)
+            AppUtils.getAppIntent(
+                this,
+                action,
+                data
+            )
 
         startActivity(activityIntent)
 
-        // Launch your Flutter CallNotificationService as well
+        // Application's CallNotificationService
         try {
+            val className =
+                "${applicationContext.packageName}.CallNotificationService"
+
             Log.d(
                 "TransparentActivity",
-                "Launching CallNotificationService"
+                "Launching $className"
             )
 
-            val notificationIntent = Intent(
-                this,
-                CallNotificationService::class.java
-            )
+            val activityClass =
+                Class.forName(className)
+
+            val notificationIntent =
+                Intent(
+                    this,
+                    activityClass
+                )
+
+            notificationIntent.action = action
+
+            if (data != null) {
+                notificationIntent.putExtra(
+                    FlutterCallkitIncomingPlugin.EXTRA_CALLKIT_CALL_DATA,
+                    data
+                )
+            }
 
             notificationIntent.addFlags(
                 Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
-                Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK
             )
 
             startActivity(notificationIntent)
