4c38df3ffa0d2e153f95a60ea33a840c67b87325
mechtech-mind <aayushsaurabh02@gmail.com>
2026-08-12 18:13:13 +0530

    Transparent activity restored

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/TransparentActivity.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/TransparentActivity.kt
@@ -11,28 +11,19 @@ class TransparentActivity : Activity() {
     companion object {
         var isVisible: Boolean = false
 
-        fun getIntent(
-            context: Context,
-            action: String,
-            data: Bundle?
-        ): Intent {
-            val intent = Intent(
-                context,
-                TransparentActivity::class.java
-            )
-
+        fun getIntent(context: Context, action: String, data: Bundle?): Intent {
+            val intent = Intent(context, TransparentActivity::class.java)
             intent.action = action
             intent.putExtra("data", data)
-
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
             intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-
             return intent
         }
     }
+
 
     override fun onStart() {
         super.onStart()
@@ -43,84 +34,20 @@ class TransparentActivity : Activity() {
         super.onCreate(savedInstanceState)
 
         val action = intent.action
-
         if (action == null) {
-            Log.w(
-                "TransparentActivity",
-                "Intent action is null, finishing activity"
-            )
+            Log.w("TransparentActivity", "Intent action is null, finishing activity")
             finish()
             return
         }
 
         val data = intent.getBundleExtra("data")
 
-        // Original flutter_callkit_incoming flow
-        val broadcastIntent =
-            CallkitIncomingBroadcastReceiver.getIntent(
-                this,
-                action,
-                data
-            )
-
-        broadcastIntent.addFlags(
-            Intent.FLAG_RECEIVER_FOREGROUND
-        )
-
+        val broadcastIntent = CallkitIncomingBroadcastReceiver.getIntent(this, action, data)
+        broadcastIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND)
         sendBroadcast(broadcastIntent)
 
-        // Original application Activity
-        val activityIntent =
-            AppUtils.getAppIntent(
-                this,
-                action,
-                data
-            )
-
+        val activityIntent = AppUtils.getAppIntent(this, action, data)
         startActivity(activityIntent)
-
-        // Application's CallNotificationService
-        try {
-            val className =
-                "${applicationContext.packageName}.CallNotificationService"
-
-            Log.d(
-                "TransparentActivity",
-                "Launching $className"
-            )
-
-            val activityClass =
-                Class.forName(className)
-
-            val notificationIntent =
-                Intent(
-                    this,
-                    activityClass
-                )
-
-            notificationIntent.action = action
-
-            if (data != null) {
-                notificationIntent.putExtra(
-                    FlutterCallkitIncomingPlugin.EXTRA_CALLKIT_CALL_DATA,
-                    data
-                )
-            }
-
-            notificationIntent.addFlags(
-                Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
-                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK
-            )
-
-            startActivity(notificationIntent)
-
-        } catch (e: Exception) {
-            Log.e(
-                "TransparentActivity",
-                "Failed to launch CallNotificationService",
-                e
-            )
-        }
 
         finish()
         overridePendingTransition(0, 0)
